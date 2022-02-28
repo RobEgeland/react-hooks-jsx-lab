c4a9b31d785422181a23af9ae716909ff42955ca
Robert Egeland <robegeland@yahoo.com>
2022-02-28 17:26:57 -0600

    commit tree 910c2a9720873d8f552a91adaa090e84628b04de

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>This is the section about me so I will tell you about myself...blah blah blah</p>
+    <img src={ image } alt="I made this"></img>
+  </div>;
 }
 
 export default About;
